f32af44e62533596684ebea73d408bde7f967bea
Laura Guillory <laura.guillory@griffithuni.edu.au>
2018-06-22 10:37:35 +1000

    Fixed missing constraints for properties with datatype xsd:string

--- form-generator/rendering/__init__.py
+++ form-generator/rendering/__init__.py
@@ -2,18 +2,20 @@ import os
 from jinja2 import FileSystemLoader, Environment
 
 URIs = {
-    "NUMBER": [
-        "http://www.w3.org/2001/XMLSchema#integer",
-        "http://www.w3.org/2001/XMLSchema#float",
-        "http://www.w3.org/2001/XMLSchema#double",
-        "http://www.w3.org/2001/XMLSchema#decimal"
+    'NUMBER': [
+        'http://www.w3.org/2001/XMLSchema#integer',
+        'http://www.w3.org/2001/XMLSchema#float',
+        'http://www.w3.org/2001/XMLSchema#double',
+        'http://www.w3.org/2001/XMLSchema#decimal'
     ],
-    "EMAIL": "http://xmlns.com/foaf/0.1/mbox",
-    "DATE": "http://www.w3.org/2001/XMLSchema#date",
-    "PHONE_NUMBER": "http://xmlns.com/foaf/0.1/phone",
-    "TIME": "http://www.w3.org/2001/XMLSchema#time",
-    "BOOLEAN": "http://www.w3.org/2001/XMLSchema#boolean"
+    'EMAIL': 'http://xmlns.com/foaf/0.1/mbox',
+    'DATE': 'http://www.w3.org/2001/XMLSchema#date',
+    'PHONE_NUMBER': 'http://xmlns.com/foaf/0.1/phone',
+    'TIME': 'http://www.w3.org/2001/XMLSchema#time',
+    'BOOLEAN': 'http://www.w3.org/2001/XMLSchema#boolean',
+    'STRING': 'http://www.w3.org/2001/XMLSchema#string'
 }
+
 
 def render_template(form_name, shape):
     loader = FileSystemLoader(searchpath=os.path.dirname(__file__))
