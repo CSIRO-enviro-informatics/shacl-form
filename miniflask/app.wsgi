81f10aecfbc84625a969ad86240edf9d47208662
Laura Guillory <laura.guillory@griffithuni.edu.au>
2018-07-20 15:29:11 +1000

    Seperated out example Flask server so this is just the Python package now

--- miniflask/app.wsgi
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, '/var/www/miniflask')
-
-from app import app as application
--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/CSIRO-enviro-informatics/shacl-form',
     packages=setuptools.find_packages(),
+    install_requires=['rdflib'],
     classifiers=(
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent',
